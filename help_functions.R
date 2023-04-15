3fba6668cb89d66e11598dd991563433a991c599
Signý <signy@Signys-MacBook-Pro.local>
2023-04-15 12:41:37 +0200

    update

--- help_functions.R
+++ help_functions.R
@@ -101,4 +101,31 @@ get_periods_stats <- function(){
     return(period_data)
 }
 
+get_timeout_stats <- function(){
+    timeouts <- data %>% select(time_outs, game) %>% 
+        unique() %>% summarise("most timeouts"=max(time_outs), 
+                               "average timeouts" = mean(time_outs),
+                               "total timeouts" = sum(time_outs))
+    return(timeouts)
+}
+
+print_player_stats <- function(player_name){
+    cat("----------------------\n")
+    cat(paste0(player_name, "\n"))
+    cat("----------------------\n")
+    
+    plot_player_points(player_name)
+    ggsave(file=paste0("plots/", player_name, "_points.png"), width = 8, height = 5)
+    
+    freethrow_rate <- get_freethrow_stats(player_name)
+    cat(paste0("Freethrow rate: ", 100*(get_freethrow_stats(player_name) %>% round(.,4)), "%\n"))
+    cat(paste0("Number of games played: ", get_number_of_games(player_name), "\n"))
+    cat(paste0("In starting five: ", starting_five(player_name), "\n"))
+    cat(paste0("Number of games with more fouls than points: ", more_fouls_than_points(player_name), "\n"))
+    get_point_stats(player_name) %>% knitr::kable() %>% print()
+    get_foul_stats(player_name) %>% knitr::kable() %>% print()
+    get_time_stats(player_name) %>% knitr::kable() %>% print()
+    cat("----------------------")
+}
+
 
--- pandas.R
+++ pandas.R
@@ -12,7 +12,7 @@ period_data <- data_list[[2]]
 coach_data <- data_list[[3]]
 summary <- data_list[[4]]
 
-data <- clean_data(data_raw, summary)
+data <- clean_data(data_raw, period_data, coach_data, summary)
 
 # Total stats
 ## Plots
@@ -24,7 +24,7 @@ grid.arrange(grobs=game_plots, ncol=2)
 plot1 <- arrangeGrob(grobs=game_plots, ncol=2)
 ggsave(file="plots/plot1.png", plot1, width = 12, height = 12)
 
-plot2 <- plot_periods_boxplot()
+(plot2 <- plot_periods_boxplot())
 ggsave(file="plots/plot2.png", plot2, width = 8, height = 5)
 
 ## Stats
@@ -33,23 +33,12 @@ cat(paste0("Freethrow rate: ", 100*(get_freethrow_stats() %>% round(.,4)), "%"))
 get_point_stats()
 get_foul_stats()
 get_periods_stats()
-
-
+get_timeout_stats()
 
 # Per player
 players <- data %>% pull(player) %>% unique()
-player_name <- "Alexandra"
-
-plot_player_points(player_name)
-freethrow_rate <- get_freethrow_stats(player_name)
-cat(paste0("Freethrow rate: ", 100*(get_freethrow_stats(player_name) %>% round(.,4)), "%"))
-cat(paste0("Number of games played: ", get_number_of_games(player_name)))
-cat(paste0("In starting five: ", starting_five(player_name)))
-cat(paste0("Number of games with more fouls than points: ", more_fouls_than_points(player_name)))
-get_point_stats(player_name)
-get_foul_stats(player_name)
-get_time_stats(player_name)
-
+player_name <- players[4]
+print_player_stats(player_name)
 
 
 
